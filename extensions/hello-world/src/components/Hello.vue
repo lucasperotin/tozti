96674f0121b400d9b58e149e01b4a79e3cda4d39
flupe <lucas@escot.me>
2017-12-01 20:14:04 +0100

    Added sample counter

--- extensions/hello-world/src/components/Hello.vue
+++ extensions/hello-world/src/components/Hello.vue
@@ -1,12 +1,25 @@
 <template>
-	<p>{{ greeting }} World!</p>
+  <div>
+    <p>{{ count }}</p>
+    <button v-on:click="increase">+1</button>
+    <button v-on:click="decrease">-1</button>
+  </div>
 </template>
 
 <script>
 	export default {
+    methods: {
+      increase: function() {
+        this.count += 1
+      },
+      decrease: function() {
+        this.count -= 1
+      }
+    },
+
 		data () {
 			return {
-				greeting: 'Hello'
+				count: 0
 			}
 		}
 	}
